970fc6eaf5c524b937ede62560e5d8998ef34491
Stefan Christan <stefan1234595@gmail.com>
2023-03-22 22:49:54 +0100

    feat: use current date, replace placeholders of Serial plot with functionality

--- RoomAvailability.ino
+++ RoomAvailability.ino
@@ -9,70 +9,69 @@
 #include "timetable.h"
 #include "wlan_credentials.h"
 
-#define TIME_TO_SLEEP_SHORT 30      // Time ESP32 will go to sleep before resetting to current week after Touchpad action (in seconds)
-#define TIME_TO_RELEASE_TOUCHPAD 3  // Time ESP32 will go to sleep before resetting to current week after Touchpad action (in seconds)
+#define TIME_TO_RELEASE_TOUCHPAD 1  // Time ESP32 will go to sleep before resetting to current week after Touchpad action (in seconds)
 
 Inkplate display(INKPLATE_1BIT);
 
-RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR int8_t week_offset = 0;
 
 void setup() {
   Serial.begin(115200);
   display.begin();
-  display.setRotation(2);
 
   prepareDisplayPins();
-  ++bootCount;
-  displayInfo();
-  display.clearDisplay();
-  performUpdate();
 
-  esp_sleep_wakeup_cause_t wakeup_reason;
-  wakeup_reason = esp_sleep_get_wakeup_cause();
+  time_t timetable_time = display.rtcGetEpoch() + week_offset * time_t(7 * 24 * 60 * 60);
   bool today_is_set = true;
+  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
 
   if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT1) {
     // Check the touchpads 10 times with a short delay in beetween
     bool touchpad_pressed = false;
-    for (uint8_t i = 0; i < 100; i++) {
+    for (uint8_t i = 0; i < 200; i++) {
       // Check if the first touchpad has been touched.
       if (display.readTouchpad(PAD1)) {
         Serial.println("Pressed Touchpad 1!");
-        delay(2000);  // time to do some tasks which need 500ms
+        week_offset--;
+        timetable_time = timetable_time - time_t(7 * 24 * 60 * 60);
         touchpad_pressed = true;
         today_is_set = false;
       }
       // Check if the second touchpad has been touched.
       else if (display.readTouchpad(PAD2)) {
         Serial.println("Pressed Touchpad 2!");
-        delay(2000);  // time to do some tasks which need 500ms
+        week_offset = 0;
+        timetable_time = display.rtcGetEpoch();
         touchpad_pressed = true;
         today_is_set = true;
       }
       // Check if the third touchpad has been touched.
       else if (display.readTouchpad(PAD3)) {
         Serial.println("Pressed Touchpad 3!");
-        delay(2000);  // time to do some tasks which need 500ms
+        week_offset++;
+        timetable_time = timetable_time + time_t(7 * 24 * 60 * 60);
         touchpad_pressed = true;
         today_is_set = false;
       }
       // If a touchpad has been pressed, wait a short time for the user to release the touchpad.
       if (touchpad_pressed) {
-        Serial.println("Display was refreshed, taking a break...");
+        performUpdate(timetable_time);
+        Serial.println("Display was refreshed, time to release touchpads...");
         delay(TIME_TO_RELEASE_TOUCHPAD * 1000);
         // Clear the touchpads
         display.readTouchpad(PAD1);
         display.readTouchpad(PAD2);
         display.readTouchpad(PAD3);
         touchpad_pressed = false;
-        //break;
       } else {
         delay(300);
       }
     }
   } else {
-    Serial.println("Pressed Touchpad 2! -- today");
-    delay(2000);  // time to do some tasks which need 500ms
+    Serial.println("Auto reset to today!");
+    timetable_time = display.rtcGetEpoch();
+    week_offset = 0;
+    performUpdate(timetable_time);
   }
 
   if (today_is_set) {
@@ -113,63 +112,26 @@ void prepareDisplayPins() {
   display.setIntPin(PAD3, RISING, IO_INT_ADDR);
 }
 
-void performUpdate() {
-  MyWiFi wifi(display, WLAN_SSID, WLAN_PASSWORD);
-  wifi.connect();
+void performUpdate(time_t time_epoch) {
+  static bool wifi_is_connected = false;
+  if (!wifi_is_connected) {
+    display.clearDisplay();
+    //MyWiFi wifi(display, WLAN_SSID_WPA2, WLAN_IDENTITY_WPA2, WLAN_USERNAME_WPA2, WLAN_PASSWORD_WPA2);
+    MyWiFi wifi(display, WLAN_SSID, WLAN_PASSWORD);
+    wifi.connect();
+    wifi_is_connected = true;
+  }
   display.clearDisplay();
 
-  display.setCursor(600, 5);
-  display.rtcGetRtcData();
-  display.print(String(display.rtcGetHour()) + ":" + String(display.rtcGetMinute()) + ":" + String(display.rtcGetSecond()) + "  -  " + String(display.rtcGetEpoch()));
-  Serial.println("RTC Epoch: " + String(display.rtcGetEpoch()));
-  display.display();
-
-  Timetable timetable(display, 457, 1668423349 + 3600 * 24);  //display_.rtcGetEpoch()); // 457 663
+  Timetable timetable(display, 457, time_epoch);  // 457 663
   timetable.drawBackground();
   timetable.drawTimesAndDays();
   bool data_loaded_successfully = timetable.drawData();
-  if (data_loaded_successfully) {
-    display.display();
+  if (!data_loaded_successfully) {
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setCursor(5, 5);
+    display.println("Der Raumbelegungsplan konnte nicht geladen werden, bitte versuchen Sie es erneut.");
   }
-}
-
-void displayInfo() {
-  // First, lets delete everything from frame buffer
-  display.clearDisplay();
-
-  // Set text cursor and size
-  display.setCursor(10, 280);
-  display.setTextSize(2);
-
-  display.print(F("Boot count: "));
-  display.println(bootCount, DEC);  // Print the number
-
-  // Set next line cursor position
-  display.setCursor(10, 320);
-
-  // Display wake up reason
-  esp_sleep_wakeup_cause_t wakeup_reason;
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-  switch (wakeup_reason) {
-    case ESP_SLEEP_WAKEUP_EXT0:
-      display.println("Wakeup caused by external signal using RTC_IO");
-      break;
-    case ESP_SLEEP_WAKEUP_EXT1:
-      display.println("Wakeup caused by external signal using RTC_CNTL");
-      break;
-    case ESP_SLEEP_WAKEUP_TIMER:
-      display.println("Wakeup caused by timer");
-      break;
-    case ESP_SLEEP_WAKEUP_TOUCHPAD:
-      display.println("Wakeup caused by touchpad");
-      break;
-    case ESP_SLEEP_WAKEUP_ULP:
-      display.println("Wakeup caused by ULP program");
-      break;
-    default:
-      display.println("Wakeup was not caused by deep sleep");
-      break;
-  }
-
   display.display();
 }
